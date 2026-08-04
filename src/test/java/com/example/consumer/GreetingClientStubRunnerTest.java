b9bcbee6178547a9029715c105a503000f368536
Adam-Lemdani <Adam.Lemdani@ii.co.uk>
2026-08-04 10:33:33 +0100

    Updates - more intuitive demo testing & pattern matching for correlating branches.
    I.E
    PR raised here titled SIPP2-38 -> Downstream dependency contracts break -> raise PR in alternate repo titled SIPP2-38 -> it'll execute the tests against this matching branch.

--- src/test/java/com/example/consumer/GreetingClientStubRunnerTest.java
+++ src/test/java/com/example/consumer/GreetingClientStubRunnerTest.java
@@ -20,7 +20,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @AutoConfigureStubRunner
 class GreetingClientStubRunnerTest {
-
     @Autowired
     private StubFinder stubFinder;
 
